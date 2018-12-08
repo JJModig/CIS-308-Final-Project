c37cc6dd5b63ac213f4c4d79e13a1a29b332a39f
Jonathan Modig <jjmodig@cougar.cs.ksu.edu>
2018-12-08 17:13:45 -0600

    Changed List to implement addresses of strings

--- list.h
+++ list.h
@@ -4,31 +4,42 @@
 #include <stdlib.h>
 //This implements a dynamic array.
 //Solution taken from https://stackoverflow.com/questions/3536153/c-dynamically-growing-array
-typedef struct {
-  int *array;
+//and modified for strings: maximum 15 characters
+#define STR_LEN 15
+typedef struct List{
+  char **array;
   size_t used;
   size_t size;
-} Array;
+} List;
 
-void initArray(Array *a, size_t initialSize) {
-  a->array = (int *)malloc(initialSize * sizeof(int));
+void initList(List *a, size_t initialSize) {
+  	a->array = malloc(initialSize * sizeof(char*));
+	for(int i=0; i<initialSize; i++) {
+		a->array[i] = malloc((STR_LEN+1) * sizeof(char));
+	}
   a->used = 0;
   a->size = initialSize;
 }
 
-void insertArray(Array *a, int element) {
+void insertList(List *a, char *element) {
   // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
   // Therefore a->used can go up to a->size 
   if (a->used == a->size) {
-    a->size *= 2;
-    a->array = (int *)realloc(a->array, a->size * sizeof(int));
+    	a->size *= 2;
+    	a->array = realloc(a->array, a->size * sizeof(char*));
+	for(int i=(a->size)/2; i< a->size; i++) {
+		a->array[i] = malloc((STR_LEN+1) * sizeof(char));
+	}
   }
   a->array[a->used++] = element;
 }
 
-void freeArray(Array *a) {
-  free(a->array);
-  a->array = NULL;
-  a->used = a->size = 0;
+void freeList(List *a) {
+	for(int i=0; i < a->size; i++) {
+		free(a->array[i]);
+	}
+  	free(a->array);
+  	a->array = NULL;
+  	a->used = a->size = 0;
 }
 #endif // list_h
